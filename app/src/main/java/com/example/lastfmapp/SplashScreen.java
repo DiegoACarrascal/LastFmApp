f0fcd30264a6feb4a8096e3e81c04a8ecb5ea865
DiegoACarrascal <diegoar_1920@hotmail.com>
2023-04-09 11:49:52 -0500

    IMPLEMENTACIÓN - Permisos del aplicativo
    * Se agrega clase que se encarga de solicitar los permisos del aplicativo
    * Se agregan permisos de localicacion y para acceder a la multimedia del dispositivo

--- app/src/main/java/com/example/lastfmapp/SplashScreen.java
+++ app/src/main/java/com/example/lastfmapp/SplashScreen.java
@@ -7,19 +7,45 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
+import com.example.lastfmapp.Tools.PermissionStatus;
 import com.example.lastfmapp.View.PopularArtists;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private PermissionStatus permissionStatus;
+    private Boolean isIntent=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(() -> {
-            Log.i("SplashScreen", "Inicio app");
-            Intent i = new Intent(SplashScreen.this, PopularArtists.class);
-            startActivity(i);
-            finish();
-        },3000);
+        permissionStatus = new PermissionStatus(SplashScreen.this, this);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (permissionStatus.firstTry) {
+            permissionStatus.confirmPermissionMsg();
+        } else {
+            permissionStatus.reqPermissions();
+        }
+
+        if(permissionStatus.validatePermissions() && isIntent){
+            new Handler().postDelayed(() -> {
+                Log.i("SplashScreen", "Inicio app");
+                Intent i = new Intent(SplashScreen.this, PopularArtists.class);
+                startActivity(i);
+                finish();
+            },2000);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isIntent=true;
+    }
+
 }
